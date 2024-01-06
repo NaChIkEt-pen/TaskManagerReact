3c03743576e83f31f633431796106f5e2b989ee2
Nachiket Pensalwar <116742714+NaChIkEt-pen@users.noreply.github.com>
2024-01-06 11:29:51 +0530

    Added login navigate

--- Frontend/src/Router/Router.jsx
+++ Frontend/src/Router/Router.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import AdminLogin from "../pages/AdminPages/AdminLogin";
 import React from "react";
+import AdminDashboard from "../pages/AdminPages/AdminDashboard";
 
 function MyRouter() {
   return (
     <Routes>
       <Route path="/admin/login" element={<AdminLogin />} />
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
     </Routes>
   );
 }
--- Frontend/src/pages/AdminPages/AdminLogin.jsx
+++ Frontend/src/pages/AdminPages/AdminLogin.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { ReactDOM } from "react";
+import AdminDashboard from "./AdminDashboard";
+import { useNavigate } from "react-router-dom";
 
 function AdminLogin() {
   const [logindata, setLoginData] = useState([]);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const navigate = useNavigate();
+
   useEffect(() => {
     fetch("http://localhost:3000/admin/details")
       .then((res) => res.json())
@@ -12,37 +17,49 @@ function AdminLogin() {
       })
       .catch((err) => console.log(err));
   }, []);
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs.empID);
+    if (inputs.empID == "nachiket") {
+      setFormSubmitted(true);
+      navigate("/admin/dashboard");
+    }
+  };
   return (
     <>
-      <div className="Auth-form-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Sign In</h3>
-            <div className="form-group mt-3">
-              <label>Employee ID</label>
-              <input
-                type="text"
-                className="form-control mt-1"
-                placeholder="Enter ID"
-              />
-            </div>
-            <div className="form-group mt-3">
-              <label>Password</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Enter password"
-                autoComplete="on"
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your ID:
+          <input
+            type="text"
+            name="empID"
+            value={inputs.empID || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Enter your password:
+          <input
+            type="password"
+            name="password"
+            autoComplete="true"
+            value={inputs.password || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+
+      {/* {formSubmitted && <AdminDashboard />} */}
     </>
   );
 }
